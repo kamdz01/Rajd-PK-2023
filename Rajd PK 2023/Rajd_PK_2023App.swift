82e21a1ff6ea19b00a1c6b45a4f279e9d3d91ed9
Kamil Dziedzic <kamdz01@gmail.com>
2023-01-11 21:11:15 +0100

    Initial Commit

---
+++ Rajd PK 2023/Rajd_PK_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Rajd_PK_2023App.swift
+//  Rajd PK 2023
+//
+//  Created by Kamil Dziedzic on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rajd_PK_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
